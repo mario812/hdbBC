cc9763fc1d7417bcec6df73e1afaa9c06a632ecc
@mario812 <mlct_018@hotmail.com>
2024-02-14 09:13:48 -0400

    chore: puedo borrar cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'miappnx-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  template: `
+  <header>
+    <h1> Hola, soy yo</h1>
+  </header>`
+ 
 })
 export class AppComponent {
-  title = 'miappnx';
+
 }
