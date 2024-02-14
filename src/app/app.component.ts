5c77b247839876908461f6c67d971f4a40210090
@mario812 <mlct_018@hotmail.com>
2024-02-14 10:54:30 -0400

    feat: integrar el wallet adapter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HdWalletMultiButtonComponent],
   selector: 'miappnx-root',
   template: `
-  <header>
-    <h1> Hola, soy yo</h1>
+  <header class="py-8">
+    <h1 class="text-5xl text-center mb-4"> MI WALLET</h1>
+<div class="flex justify-center">
+    <hd-wallet-multi-button>
+
+    </hd-wallet-multi-button>
+</div>
   </header>`
  
 })
