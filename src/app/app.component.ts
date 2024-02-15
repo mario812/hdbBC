0113ed2bc45e3331c50feac0c0fe01f5fc2ea8d5
@mario812 <mlct_018@hotmail.com>
2024-02-15 17:14:12 -0400

    feat: display token value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,22 @@ import { WalletStore } from '@heavy-duty/wallet-adapter';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { computedAsync } from 'ngxtension/computed-async';
 
+
 @Component({
   standalone: true,
   imports: [RouterModule, HdWalletMultiButtonComponent],
   selector: 'miappnx-root',
-  template: ` <header class="py-8">
+  template: ` <header class="py-8 relative">
     <h1 class="text-5xl text-center mb-4">MI WALLET</h1>
     <div class="flex justify-center">
       <hd-wallet-multi-button> </hd-wallet-multi-button>
     </div>
 
     @if (account()) {
-      <div>hasjkkksdd</div>
+      <div class="absolute top-4 left-4 flex justify-center items-center gap-2">
+        <img [src] = "account()?.info?.image" class = "w-8 h-8" />
+        <p class="text x-l"> {{account()?.balance}}</p>
+      </div>
     }
   </header>`,
 })
