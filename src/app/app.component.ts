db03eb5a8156dc5c30219375c8d182d803635fc0
@mario812 <mlct_018@hotmail.com>
2024-02-15 09:05:46 -0400

    chore: instalacion de servicios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
+import { ShyftApiService } from './shyft-api.service';
+import { WalletStore } from '@heavy-duty/wallet-adapter';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { computedAsync } from 'ngxtension/computed-async';
 
 @Component({
   standalone: true,
   imports: [RouterModule, HdWalletMultiButtonComponent],
   selector: 'miappnx-root',
-  template: `
-  <header class="py-8">
-    <h1 class="text-5xl text-center mb-4"> MI WALLET</h1>
-<div class="flex justify-center">
-    <hd-wallet-multi-button>
+  template: ` <header class="py-8">
+    <h1 class="text-5xl text-center mb-4">MI WALLET</h1>
+    <div class="flex justify-center">
+      <hd-wallet-multi-button> </hd-wallet-multi-button>
+    </div>
 
-    </hd-wallet-multi-button>
-</div>
-  </header>`
- 
+    @if (account()) {
+      <div>hasjkkksdd</div>
+    }
+  </header>`,
 })
 export class AppComponent {
+  private readonly shyftApiService = inject(ShyftApiService);
+  private readonly _walletStore = inject(WalletStore);
+  private readonly _publicKey = toSignal(this._walletStore.publicKey$);
 
+  readonly account = computedAsync(
+    () => this.shyftApiService.getAccount(this._publicKey()?.toBase58()),
+    { requireSync: true },
+  );
 }
