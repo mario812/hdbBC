7a1b3c614d0fa656a77e7ad912f05cf75b3a14cd
@mario812 <mlct_018@hotmail.com>
2024-02-22 11:05:09 -0400

    feat: se creó un archivo de estilos css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,103 +10,53 @@ import { computedAsync } from 'ngxtension/computed-async';
   standalone: true,
   imports: [RouterModule, HdWalletMultiButtonComponent],
   selector: 'miappnx-root',
-  template: ` 
- 
- <style>
-  .container {
-  width: 80%;
-  margin: 0 auto;
-}
+  styleUrls: ['./app.component.css'],
+  template: `
+    <div class="container">
+      <div class="header">
+        <h1>Reto 2da. Semana</h1>
+        <hd-wallet-multi-button> </hd-wallet-multi-button>
+      </div>
 
-.header {
-  text-align: center;
-  margin-bottom: 20px;
-}
-
-.header h1 {
-  font-size: 24px;
-}
-
-.header button {
-  margin: 0 auto;
-}
-
-.balance-table, .history-table {
-  margin-top: 20px;
-}
-
-table {
-  width: 100%;
-}
-
-th, td {
-  padding: 10px;
-}
-.balance-table h2 {
-  text-align: center;
-}
-.history-table h2 {
-  text-align: center;
-}
- </style>
-
-<div class="container">
-  <div class="header">
-    <h1>Reto 2da. Semana</h1>
-    <hd-wallet-multi-button> </hd-wallet-multi-button>
-  </div>
-
- @if (account()) {
-  <div class="balance-table">
-    <h2>Página de Balance</h2>
-    <table class="table table-striped">
-      <!-- <thead>
-        <tr>
-          <th>Moneda</th>
-          <th>Balance</th>
-        </tr>
-      </thead> -->
-      <tbody>
-      <img class= "w-24 h-24" src="./assets/Solana_logo.png" />
-        {{ accountsol()?.balance }}
-      @for (item of account(); track $index) {
+      @if (account()) {
+        <div class="balance-table">
+          <h2>Página de Balance</h2>
+          <table class="table table-striped">
         
-            <img class="w-24 h-24" src="{{ item.info.image }}">
-             <p>{{ item.balance }}</p>
-    }
-      </tbody>
-    </table>
-  </div>
- }
+            <tbody>
+              <img class="w-24 h-24" src="./assets/Solana_logo.png" />
+              {{
+                accountsol()?.balance
+              }}
+              @for (item of account(); track $index) {
+                <img class="w-24 h-24" src="{{ item.info.image }}" />
+                <p>{{ item.balance }}</p>
+              }
+            </tbody>
+          </table>
+        </div>
+      }
 
-
-
-  <div class="history-table">
-    <h2>Página de Transacciones</h2>
-    @if (!transacciones()) {
-   <p> no se conectó </p>}
-    @else {
-    <table class="table table-striped">
-     
-      <tbody>
-
-      @for (item of transacciones(); track $index) {
-        <br>
-         <p>{{item.status}}</p> <p>{{item.type}}</p><p>{{item.timestamp}}</p>
-        <br>
-        
-}
-      </tbody>
-    </table>
-}
-  </div>
-
-</div>
-
-
-  
-  
-  `
+      <div class="history-table">
+        <h2>Página de Transacciones</h2>
+        @if (!transacciones()) {
+          <p>no se conectó</p>
+        } @else {
+          <table class="table table-striped">
+            <tbody>
+              @for (item of transacciones(); track $index) {
+                <br />
+                <p>{{ item.status }}</p>
+                <p>{{ item.type }}</p>
+                <p>{{ item.timestamp }}</p>
+                <br />
+              }
+            </tbody>
+          </table>
+        }
+      </div>
+    </div>
+  `,
 })
 export class AppComponent {
   private readonly shyftApiService = inject(ShyftApiService);
@@ -118,7 +68,6 @@ export class AppComponent {
     { requireSync: true },
   );
 
-  
   readonly accountsol = computedAsync(
     () =>
       this.shyftApiService.getAccountSolBalance(this._publicKey()?.toBase58()),
@@ -129,5 +78,4 @@ export class AppComponent {
     () => this.shyftApiService.getTransactions(this._publicKey()?.toBase58()),
     { requireSync: true },
   );
- 
-  }
+}
