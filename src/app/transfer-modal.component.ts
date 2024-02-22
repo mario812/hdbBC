32ac937ba4862b366727757937a3107ff37b4f00
@mario812 <mlct_018@hotmail.com>
2024-02-22 09:52:12 -0400

    feat: crear semana3

---
+++ src/app/transfer-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component ({
+    selector: 'miappnx-transfer-modal',
+    template: `
+        <h2> Transferencia de Fondos</h2>,
+
+    `
+
+})
+
+export class TransferModalComponent {
+    
+}
